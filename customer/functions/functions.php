7a8cb6e921de1b8b4269e936843fa2cf8a996896
hoanquoctoan2851 <84771529+hoanquoctoan2851@users.noreply.github.com>
2022-03-29 11:47:08 +0700

    Add files via upload

---
+++ customer/functions/functions.php
@@ -0,0 +1,37 @@
+
+
+<?php 
+
+	$db = mysqli_connect("localhost","root","","cafe_web");
+
+
+	function getRealIpUser(){
+
+		switch (true) {
+			case(!empty($_SERVER['HTTP_X_REAL_IP'])) : return $_SERVER['HTTP_X_REAL_IP'];
+
+			case(!empty($_SERVER['HTTP_CLIENT_IP'])) : return $_SERVER['HTTP_CLIENT_IP'];
+
+			case(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) : return $_SERVER['HTTP_X_FORWARDED_FOR'];
+
+			default : return $_SERVER['REMOTE_ADDR'];
+				
+		}
+  
+            }
+	
+
+ ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
